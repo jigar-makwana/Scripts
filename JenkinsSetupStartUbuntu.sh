7f1e9ea1ac7bdd10ff8775beb247b542908567b8
Jigar Mawana <jigar_makwana@student.uml.edu>
2019-11-10 15:25:09 -0600

    1st

---
+++ JenkinsSetupStartUbuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#echo "Update and upgrade all the things..."
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#echo "Some essentials..."
+sudo apt-get install -y curl wget git 
+
+git config --global user.email "########################"
+git config --global user.name "########"
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-get install -y jenkins
+
+sudo apt install -y openjdk-8-jdk
+
+sudo service jenkins start
+sudo chkconfig jenkins on
+
