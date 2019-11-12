973a74fd11a007586f0719f4c1ecec23c6b2dce7
Jigar Mawana <jigar_makwana@student.uml.edu>
2019-11-12 16:30:15 -0600

    3rd

--- Nginx/Nginx simple integration with jenkins.sh
+++ Nginx/Nginx simple integration with jenkins.sh
@@ -1,3 +1,7 @@
+# cd /etc/nginx/sites-available# vi default 
+# Nginx integration with jenkins using loopback 
+# you can disable 8080 port after this and use nginx for controlling jekins 
+# This is simple example you can add more security on this conf file.
 
 upstream jenkins {
         server 127.0.0.1:8080;
@@ -12,3 +16,4 @@ server {
                 proxy_set_header X-Real_IP $remote_addr;
                 }
 }
+#nginx -t
