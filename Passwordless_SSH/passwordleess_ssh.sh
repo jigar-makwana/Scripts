dc4b73154d803f6ca1c60c09ecd261860b44bd5b
Jigar Mawana <jigar_makwana@student.uml.edu>
2019-11-17 11:39:20 -0600

    3rd

--- Passwordless_SSH/passwordleess_ssh.sh
+++ Passwordless_SSH/passwordleess_ssh.sh
@@ -20,4 +20,13 @@ nano ~/root/.ssh/authorized_keys
 #and now you can ssh into your remote server from current server where you created public and private key
 
 
+#change the permission of authorized_key on remote host to 
+chmod 700 .ssh; chmod 640 .ssh/authorized_keys
+
+
+# if yoy have set up a passward to ssh
+## you can copy the public with using passward to login to remote server
+
+cat .ssh/id_rsa.pub | ssh ubuntu@(____private ip____) 'cat >> .ssh/authorized_keys'
+
 
