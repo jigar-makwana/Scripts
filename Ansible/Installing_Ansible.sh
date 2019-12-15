fbb6c87df3701c61d7b0b35c184f849f3d9ece31
Jigar Mawana <jigar_makwana@student.uml.edu>
2019-12-14 20:02:15 -0600

    Update Installing_Ansible.sh

--- Ansible/Installing_Ansible.sh
+++ Ansible/Installing_Ansible.sh
@@ -1,12 +1,15 @@
+#### On Ubuntu  server
 
 #!/bin/bash
 sudo bash
+
 
 
 apt-get update -y
 apt-get upgrade -y
 
 
+apt-get install -y python3
 apt install -y python3-pip python-pip
 pip3 install awscli
 pip3 install ansible
@@ -40,9 +43,39 @@ pip3 install boto boto3
 #adding slave privae ip
 
 
-#run snsible command as a ans1 user (master)
+#run ansible command as a ans1 user (master)
 #ansible all -m ping
 
+
+
+
+ #### Install Ansible as a ansadmin user (on Control node)
+   ```sh 
+   su - ansadmin
+   pip3 install ansible --user
+   ``` 
+      Note: Ansible must be installed as a user (here ansadmin)
+1. check for ansible version 
+   ```sh
+   ansible --version
+   ```
+
+1. Log in as a ansadmin user on master and generate ssh key (on Control node)
+   ```sh 
+   ssh-keygen
+   ```
+1. Copy keys onto all ansible managed hosts (on Control node)
+   ```sh 
+   ssh-copy-id ansadmin@<target-server>
+   ```
+### Validation test
+
+1. Create a directory /etc/ansible and create an inventory file called "hosts" add control node IP address in it. 
+   
+1. Run ansible command as ansadmin user it should be successful (Master)
+   ```sh 
+   ansible all -m ping
+   ```
 
 
 
