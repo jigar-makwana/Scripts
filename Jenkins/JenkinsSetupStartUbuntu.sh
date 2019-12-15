f54652c61cef98da5b7ff0d4dc69878da0a93b67
Jigar Mawana <jigar_makwana@student.uml.edu>
2019-12-14 18:09:19 -0600

    Update JenkinsSetupStartUbuntu.sh

--- Jenkins/JenkinsSetupStartUbuntu.sh
+++ Jenkins/JenkinsSetupStartUbuntu.sh
@@ -12,8 +12,8 @@ sudo apt-get install -y curl wget git
 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update -y
-sudo apt-get install -y jenkins
+sudo apt-get update
+sudo apt-get install jenkins
 
 sudo apt install -y openjdk-8-jdk
 
