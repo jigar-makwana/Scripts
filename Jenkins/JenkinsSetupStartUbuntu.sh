c103660d98b7ad56fd44b83ff44ae6500b2dfff5
Jigar Mawana <jigar_makwana@student.uml.edu>
2019-12-14 18:32:47 -0600

    Update JenkinsSetupStartUbuntu.sh

--- Jenkins/JenkinsSetupStartUbuntu.sh
+++ Jenkins/JenkinsSetupStartUbuntu.sh
@@ -16,6 +16,7 @@ sudo apt-get update
 sudo apt-get install jenkins
 
 sudo apt install -y openjdk-8-jdk
+sudo apt-get install -y maven
 
 #https://support.cloudbees.com/hc/en-us/articles/216118748-How-to-Start-Stop-or-Restart-your-Instance-
 #sudo service jenkins start
@@ -25,8 +26,19 @@ sudo chkconfig jenkins on
 
 
 
+# now after installing nginx you can start setting up java and maven path in jenkins
 
 
+#  echo $JAVA_HOME
+
+#Display JAVA_HOME variable path.
+#IF NOTHING APPEARS THEN SET IT WITH THIS
+
+#  export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+
+
+# check maven's path by
+# mvn -v
 
 
 
